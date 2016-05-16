46a23d6ac1b988bd1ec5398fe7a2f46cf54bca5c
Joe Mills <mills.joe@gmail.com>
2016-05-16 15:06:07 -0500

    Example config file

---
+++ example_config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    path: '../traffic-2016-05-13-a.json', // path to the file to replay
+    host: 'localhost', // the host to replay to
+    port: '3000', // the port to replay on
+    mutate_session: true, // do we mutate the session? this re
+    debug: true,
+    processors: [
+        require('./processors/json_date_forwarding')
+    ]
+};
