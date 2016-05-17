62305e500f4a62f2f4db71481236af6639f2a527
Clay Gulick <claytongulick@gmail.com>
2016-05-16 21:12:47 -0500

    added example for session id

--- example_config.js
+++ example_config.js
@@ -3,6 +3,7 @@ module.exports = {
     host: 'localhost', // the host to replay to
     port: '3000', // the port to replay on
     mutate_session: true, // do we mutate the session? this re
+    session_key: "healthelt_sid",
     debug: true,
     processors: [
         require('./processors/json_date_forwarding')({})
